139614e84fead44fd9766142bdb6642c958e9066
bjl0615 <bjl0615@gmail.com>
2023-01-09 23:39:11 +0900

    Spring_API

---
+++ pro03/src/com/spring/ex01/Calculator.java
@@ -0,0 +1,24 @@
+package com.spring.ex01;
+
+public class Calculator {
+	
+	public void add(int x , int y) {
+		int result = x + y;
+		System.out.println("결과 : " + result);
+	}
+	
+	public void substract(int x , int y) {
+		int result = x - y;
+		System.out.println("결과 : " + result);
+	}
+	
+	public void multiple(int x , int y) {
+		int result = x * y;
+		System.out.println("결과 : " + result);
+	}
+	
+	public void divide(int x , int y) {
+		int result = x / y;
+		System.out.println("결과 : " + result);
+	}
+}
