1929d5f749eaccf5b06cc27c963739e31dfb4b7a
bjl0615 <bjl0615@gmail.com>
2023-01-18 17:18:44 +0900

    Spring_@Autowired_Members

--- pro09/src/main/java/com/spring/ex01/MainController.java
+++ pro09/src/main/java/com/spring/ex01/MainController.java
@@ -11,19 +11,19 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller("mainController")
 @RequestMapping("/test")
 public class MainController {
-	@RequestMapping(value="/main1.do" ,method=RequestMethod.GET)
-	public ModelAndView main1(HttpServletRequest request , HttpServletResponse response) throws Exception {
-		ModelAndView mav = new ModelAndView();
-		mav.addObject("msg" , "main1");
-		mav.setViewName("main");
-		return mav;
-	}
-	
-	@RequestMapping(value="/main2.do" ,method = RequestMethod.GET)
+   @RequestMapping(value="/main1.do" ,method=RequestMethod.GET)
+   public ModelAndView main1(HttpServletRequest request, HttpServletResponse response)  throws Exception{
+      ModelAndView mav=new ModelAndView();
+      mav.addObject("msg","main1");
+      mav.setViewName("main");
+      return mav;
+   }
+
+   @RequestMapping(value="/main2.do" ,method = RequestMethod.GET)
    public ModelAndView main2(HttpServletRequest request, HttpServletResponse response) throws Exception{
       ModelAndView mav=new ModelAndView();
       mav.addObject("msg","main2");
       mav.setViewName("main");
       return mav;
-	}
+   }
 }
